0e499e2b5fb0bf826f729a0ae9e06eade8860b95
lucioacri <lucioacri@gmail.com>
2025-05-29 15:56:39 +0200

    add checkbox

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 function App() {
-  const originalFormData = { author: "", title: "", text: "" };
+  const originalFormData = { author: "", title: "", body: "", public: false };
   const [formData, setFormData] = useState([]);
   const [newPost, setNewPost] = useState(originalFormData);
 
   const handleChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, checked } = e.target;
     setNewPost({
       ...newPost,
-      [name]: value,
+      [name]: name === "public" ? checked : value,
     });
   };
 
@@ -26,10 +26,6 @@ function App() {
         console.log(res.data);
       });
   };
-
-  useEffect(() => {
-    console.log(formData);
-  }, [formData]);
 
   return (
     <main>
@@ -62,20 +58,33 @@ function App() {
                 onChange={handleChange}
               />
             </div>
-            <div className="col">
+            <div className="col-12">
               <label htmlFor="post-text-area" className="form-label">
                 Testo
               </label>
 
               <textarea
                 id="post-text-area"
-                name="text"
+                name="body"
                 className="form-control"
-                value={newPost.text}
+                value={newPost.body}
                 onChange={handleChange}
               ></textarea>
-              <button className="btn btn-primary">Invia</button>
             </div>
+            <div className="col">
+              <div className="form-check">
+                <label htmlFor="post-public-input">Make the post public</label>
+                <input
+                  id="post-public-input"
+                  name="public"
+                  type="checkbox"
+                  className="form-check-input"
+                  checked={newPost.public}
+                  onChange={handleChange}
+                />
+              </div>
+            </div>
+            <button className="btn btn-primary">Invia</button>
           </div>
         </form>
       </div>
